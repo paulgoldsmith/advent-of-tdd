9185a00598dbc751696ee1d761d6c752d37529c2
Paul Goldsmith <paul.goldsmith@aplink.net>
2023-11-29 17:40:18 +0000

    Finished exercise 1 part 2

--- src/test/java/org/advent/day1/TestElfShould.java
+++ src/test/java/org/advent/day1/TestElfShould.java
@@ -72,4 +72,60 @@ public class TestElfShould {
         // Act / Assert
         assertThat(elf1.compareTo(elf2), lessThan(0));
     }
+
+    @Test
+    void elf1_test_with_1000_and_then_2000_and_then_3000_calories_added() {
+
+        Elf elf1 = new Elf();
+
+        //Act
+        elf1.addCalories(1000);
+        elf1.addCalories(2000);
+        elf1.addCalories(3000);
+
+        assertThat(elf1.getTotalCalories(), equalTo(6000));
+    }
+
+    @Test
+    void elf2_test_with_4000_calories_added() {
+
+        Elf elf2 = new Elf();
+
+        elf2.addCalories(4000);
+
+        assertThat(elf2.getTotalCalories(), equalTo(4000));
+    }
+
+    @Test
+    void elf3_test_with_5000_and_then_6000_calories_added() {
+
+        Elf elf3 = new Elf();
+
+        elf3.addCalories(5000);
+        elf3.addCalories(6000);
+
+        assertThat(elf3.getTotalCalories(), equalTo(11000));
+    }
+
+    @Test
+    void elf4_test_with_7000_and_then_8000_and_then_9000_calories_added() {
+
+        Elf elf4 = new Elf();
+
+        elf4.addCalories(7000);
+        elf4.addCalories(8000);
+        elf4.addCalories(9000);
+
+        assertThat(elf4.getTotalCalories(), equalTo(24000));
+    }
+
+    @Test
+    void elf5_test_with_10000_calories_added() {
+
+        Elf elf5 = new Elf();
+
+        elf5.addCalories(10000);
+
+        assertThat(elf5.getTotalCalories(), equalTo(10000));
+    }
 }
