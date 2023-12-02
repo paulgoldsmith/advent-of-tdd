59c3d80ad892187f70f46d116513e98085c5cea1
Paul Goldsmith <paul.goldsmith@aplink.net>
2023-12-02 17:35:33 +0000

    Improved Game code

--- day2/src/Game.ts
+++ day2/src/Game.ts
@@ -1,11 +1,14 @@
-export type CubeColors = 'red' | 'green' | 'blue';
-const Colors: CubeColors[] = ['red', 'green', 'blue'];
-
+enum CubeColorValues { 'red', 'green', 'blue' };
+export type CubeColors = keyof typeof CubeColorValues;
 export class Game {
-    private maxCubeColors: Map<CubeColors, number> = new Map([['red', 0],['green', 0], ['blue', 0]]);
+    private maxCubeColors: Map<CubeColors, number> = new Map([
+        ['red', 0],
+        ['green', 0],
+        ['blue', 0]]
+    );
 
     public randomCubeHandful(colorMap: Map<CubeColors, number>): void {
-        for (const color of Colors) {
+        for (const color of Object.keys(CubeColorValues) as CubeColors[]) {
             const valueFromBag = colorMap.get(color);
             const maxCubeValue = this.maxCubeColors.get(color);
             if (valueFromBag > maxCubeValue) {
