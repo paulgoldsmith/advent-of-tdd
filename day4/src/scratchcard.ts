e466bf0d74f12bc626d952c49b627291f2cf4201
Paul Goldsmith <paul.goldsmith@aplink.net>
2023-12-04 20:10:59 +0000

    Implemented solution to account for arbitrary number of lottery numbers

--- day4/src/scratchcard.ts
+++ day4/src/scratchcard.ts
@@ -1,6 +1,4 @@
-const CARD_FORMAT = /^Card \d+:([\s\d]+)\|([\s\d]+)$/
-const MY_NUMBERS = /^\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s$/;
-const LOTTERY_NUMBERS = /^\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)$/;
+const CARD_FORMAT = /^Card \d+:([\s\d]+)\|([\s\d]+)$/;
 export function sumScratchcardWins(scratchcards: string): number {
     return scratchcards.split('\n').reduce((prev, curr) => {
         if (!curr) {
@@ -10,16 +8,14 @@ export function sumScratchcardWins(scratchcards: string): number {
         if (!matched) {
             throw 'Unexpected format for scratchcards';
         }
-        const myNumbersMatch = myNumbersInput.match(MY_NUMBERS);
-        if (!myNumbersMatch) {
-            throw 'Unexpected format for my numbers';
-        }
-        const myNumbers = myNumbersMatch.splice(1).map(value => parseInt(value.trim()));
-        const lotteryNumbersMatch = lotteryNumbersInput.match(LOTTERY_NUMBERS);
-        if (!lotteryNumbersMatch) {
-            throw 'Unexpected format for lottery umbers';
-        }
-        const lotteryNumbers = lotteryNumbersMatch.splice(1).map(value => parseInt(value.trim()));
+        const myNumbers = myNumbersInput
+            .split(' ')
+            .filter(value => value.trim().length > 0)
+            .map(value => parseInt(value.trim()));
+        const lotteryNumbers = lotteryNumbersInput
+            .split(' ')
+            .filter(value => value.trim().length > 0)
+            .map(value => parseInt(value.trim()));
         const pointsForCard = (2 ** (myNumbers.filter(number => lotteryNumbers.includes(number)).length)) >> 1;
         return prev + pointsForCard;
     }, 0);
