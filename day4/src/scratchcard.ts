6759328e4b09e2a1e093e0489fea1a4fed1896a0
Paul Goldsmith <paul.goldsmith@aplink.net>
2023-12-04 20:00:13 +0000

    Implemented functional solution to Day4-part1

--- day4/src/scratchcard.ts
+++ day4/src/scratchcard.ts
@@ -1,4 +1,26 @@
+const CARD_FORMAT = /^Card \d+:([\s\d]+)\|([\s\d]+)$/
+const MY_NUMBERS = /^\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s$/;
+const LOTTERY_NUMBERS = /^\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)$/;
 export function sumScratchcardWins(scratchcards: string): number {
-    scratchcards;
-    return 0;
+    return scratchcards.split('\n').reduce((prev, curr) => {
+        if (!curr) {
+            return prev;
+        }
+        const [matched, myNumbersInput, lotteryNumbersInput] = curr.match(CARD_FORMAT);
+        if (!matched) {
+            throw 'Unexpected format for scratchcards';
+        }
+        const myNumbersMatch = myNumbersInput.match(MY_NUMBERS);
+        if (!myNumbersMatch) {
+            throw 'Unexpected format for my numbers';
+        }
+        const myNumbers = myNumbersMatch.splice(1).map(value => parseInt(value.trim()));
+        const lotteryNumbersMatch = lotteryNumbersInput.match(LOTTERY_NUMBERS);
+        if (!lotteryNumbersMatch) {
+            throw 'Unexpected format for lottery umbers';
+        }
+        const lotteryNumbers = lotteryNumbersMatch.splice(1).map(value => parseInt(value.trim()));
+        const pointsForCard = (2 ** (myNumbers.filter(number => lotteryNumbers.includes(number)).length)) >> 1;
+        return prev + pointsForCard;
+    }, 0);
 }
